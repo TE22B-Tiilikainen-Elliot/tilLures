3e230b7de2cfdf1cb41d0064c06606164804095a
Elliot Tiilikainen <142884112+TE22B-Tiilikainen-Elliot@users.noreply.github.com>
2024-03-05 17:47:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const productList = document.getElementById("productContainer");
+const shoppingCartList = document.getElementById("ShoppingCartContainer");
 var products = [];
 var shoppingList = [];
 
 function addToCart(i) {
     shoppingList.push(products[i.id])
     localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+}
+
+function removeFromCart(i) {
+  shoppingList = JSON.parse(localStorage.getItem("shoppingList"))
+  shoppingList.splice(i.id, 1);
+  localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+  loadShoppingCart();
 }
 
 function loadProducts() {
@@ -49,9 +57,56 @@ function loadProducts() {
   });
 }
 
+function loadShoppingCart() {
+  shoppingCartList.innerHTML = "";
+  var buttonid = 0;
+  var cartitems = JSON.parse(localStorage.getItem("shoppingList"))
+  cartitems.forEach((productitem) => {
+    const product = document.createElement("div");
+    product.id = "product";
+
+    var productname = document.createElement("p");
+    var productprice = document.createElement("p");
+    var productimage = document.createElement("img");
+    var productbutton = document.createElement("button");
+
+    productname.id = "name";
+    productprice.id = "price";
+    productimage.id = "image";
+    productbutton.id = buttonid;
+    productbutton.className = "purchaseButton";
+    productbutton.addEventListener("click", function () {
+        removeFromCart(productbutton);
+    });
+
+    productname.innerHTML = productitem.name;
+    productprice.innerHTML = productitem.price + "kr";
+    productimage.src = "productImages/" + productitem.image;
+    productbutton.innerHTML = "Ta Bort";
+
+    var namePrice = document.createElement("div");
+    namePrice.appendChild(productname);
+    namePrice.appendChild(productprice);
+
+    var namepurchase = document.createElement("div");
+    namepurchase.id = "underImage";
+    namepurchase.appendChild(namePrice);
+    namepurchase.appendChild(productbutton);
+
+    product.appendChild(productimage);
+    product.appendChild(namepurchase);
+
+    shoppingCartList.appendChild(product);
+    buttonid++;
+  });
+}
+
+
 fetch("./products.json")
   .then((response) => response.json())
   .then((json) => {
     products = json.products;
     loadProducts();
   });
+
+document.onload = loadShoppingCart();
